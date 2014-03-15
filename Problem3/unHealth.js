213a02af4b4a36f453fd4aed549672fd4e4cd5dc
David Taitz <david@David-Taitzs-MacBook-Pro.local>
2014-03-15 16:52:40 -0600

    second commit

--- Problem3/unHealth.js
+++ Problem3/unHealth.js
@@ -1,4 +1,4 @@
-var bbDetail, bbOverview, dataSet, svg;
+var bbDetail, bbOverview, dataSet, svg, areaDetail;
 
 var margin = {
     top: 50,
@@ -12,6 +12,7 @@ var width = 960 - margin.left - margin.right;
 var height = 800 - margin.bottom - margin.top;
 
 var parseDate = d3.time.format("%y-%b").parse;
+
 
 bbOverview = {
     x: 0,
@@ -36,6 +37,34 @@ svg = d3.select("#visUN").append("svg").attr({
         transform: "translate(" + margin.left + "," + margin.top + ")"
     });
 
+var xOverviewScale = d3.time.scale()
+      .range([0, bbOverview.w]);
+
+var yOverviewScale = d3.scale.linear()
+      .range([bbOverview.h, 0]);
+
+var xDetailScale = d3.time.scale()
+      .range([0, bbDetail.w]);
+
+var yDetailScale = d3.scale.linear()
+      .range([bbDetail.h, 0]);
+
+var xAxisOverview = d3.svg.axis()
+      .scale(xOverviewScale)
+      .orient("bottom");
+
+var yAxisOverview = d3.svg.axis()
+      .scale(yOverviewScale)
+      .orient("left");
+
+var xAxisDetail = d3.svg.axis()
+    .scale(xDetailScale)
+    .orient("bottom");
+
+var yAxisDetail = d3.svg.axis()
+    .scale(yDetailScale)
+    .orient("left");
+
 var convertToInt = function(s) {
     return parseInt(s.replace(/,/g, ""), 10);
 };
@@ -52,49 +81,35 @@ d3.csv("womenshealth.csv", function(data) {
 
     console.log(dataSet);
 
-    createOverview(dataSet)
-    createDetail(dataSet)
+    createVis(dataSet)
 
 });
 
-createOverview = function(dataSet) {
-  var x = d3.time.scale()
-      .range([0, bbOverview.w]);
+createVis = function(dataSet) {
 
-  var y = d3.scale.linear()
-      .range([bbOverview.h, 0]);
+  var lineOverview = d3.svg.line()
+      .x(function(d) { return xOverviewScale(d.date); })
+      .y(function(d) { return yOverviewScale(d.womenshealth); });
 
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
-
-  var line = d3.svg.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.womenshealth); });
-
-  var area = d3.svg.area()
-      .x(function(d) { return x(d.date); })
+  areaOverview = d3.svg.area()
+      .x(function(d) { return xOverviewScale(d.date); })
       .y0(bbOverview.h)
-      .y1(function(d) { return y(d.womenshealth); });
+      .y1(function(d) { return yOverviewScale(d.womenshealth); });
 
   //console.log(data);
 
-    x.domain(d3.extent(dataSet, function(d) { return d.date; }));
-    y.domain(d3.extent(dataSet, function(d) { return d.womenshealth; }));
+    xOverviewScale.domain(d3.extent(dataSet, function(d) { return d.date; }));
+    yOverviewScale.domain(d3.extent(dataSet, function(d) { return d.womenshealth; }));
 
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(25," + bbOverview.h + ")")
-        .call(xAxis);
+        .call(xAxisOverview);
 
     svg.append("g")
         .attr("class", "y axis")
         .attr("transform", "translate(25,0)")
-        .call(yAxis)
+        .call(yAxisOverview)
       .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
@@ -104,38 +119,35 @@ createOverview = function(dataSet) {
     svg.append("path")
         .datum(dataSet)
         .attr("class", "line")
+        .attr("class","overviewPath")
+        .attr("class","path")
         .attr("transform", "translate(25,0)")
         .attr("class", "timeArea")
-        .attr("d", line)
-        .attr("d", area);
-}
+        .attr("d", lineOverview)
+        .attr("d", areaOverview);
 
-createDetail = function(dataSet) {
-  var x = d3.time.scale()
-      .range([0, bbDetail.w]);
+    svg.append("g")
+            .selectAll("circle")
+            .attr("class","point")
+            .data(dataSet)
+            .enter()
+            .append("circle")
+            .attr("cx", function(d) { return xOverviewScale(d.date) +25; })
+            .attr("cy", function(d) { return yOverviewScale(d.womenshealth); })
+            .attr("r", "3")
+            .style("fill", "blue")
 
-  var y = d3.scale.linear()
-      .range([bbDetail.h, 0]);
+  var lineDetail = d3.svg.line()
+      .x(function(d) { return xDetailScale(d.date); })
+      .y(function(d) { return yDetailScale(d.womenshealth); });
 
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
-
-  var line = d3.svg.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.womenshealth); });
-
-  var area = d3.svg.area()
-      .x(function(d) { return x(d.date); })
+  areaDetail = d3.svg.area()
+      .x(function(d) { return xDetailScale(d.date); })
       .y0(bbDetail.h)
-      .y1(function(d) { return y(d.womenshealth); });
+      .y1(function(d) { return yDetailScale(d.womenshealth); });
 
-    x.domain(d3.extent(dataSet, function(d) { return d.date; }));
-    y.domain(d3.extent(dataSet, function(d) { return d.womenshealth; }));
+    xDetailScale.domain(d3.extent(dataSet, function(d) { return d.date; }));
+    yDetailScale.domain(d3.extent(dataSet, function(d) { return d.womenshealth; }));
 
     var newOverview =  bbOverview.h + 50;
     var newHeight = newOverview + bbDetail.h;
@@ -144,12 +156,12 @@ createDetail = function(dataSet) {
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(25," + newHeight + ")")
-        .call(xAxis);
+        .call(xAxisDetail);
 
     svg.append("g")
         .attr("class", "y axis")
         .attr("transform", "translate(25," + newOverview + ")")
-        .call(yAxis)
+        .call(yAxisDetail)
       .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
@@ -160,11 +172,44 @@ createDetail = function(dataSet) {
     svg.append("path")
         .datum(dataSet)
         .attr("transform", "translate(25," + newOverview + ")")
+        .attr("class","detailPath")
+        .attr("class","path")
         .attr("class", "line")
-        .attr("class", "timeArea")
-        .attr("d", line)
-        .attr("d", area);
+        .attr("class", "detailArea")
+        .attr("d", lineDetail)
+        .attr("d", areaDetail);
+
+    svg.append("g")
+            .selectAll("circle")
+            .attr("class","point")
+            .data(dataSet)
+            .enter()
+            .append("circle")
+            .attr("cx", function(d) { return xDetailScale(d.date) +25; })
+            .attr("cy", function(d) { return yDetailScale(d.womenshealth)+newOverview; })
+            .attr("r", "3")
+            .style("fill", "blue")
+
+  var brush = d3.svg.brush().x(xOverviewScale).on("brush", brushed);
+
+  svg.append("g").attr("class", "brush").call(brushed)
+      .selectAll("rect").attr({
+        height: bbOverview.h,
+        transform: "translate(0,0)"
+      });
+
+
+  function brushed() {
+    xDetailScale.domain(brush.empty() ? xOverviewScale.domain() : brush.extent());
+    svg.select(".area").attr("d", areaOverview);
+    svg.select(".x.axis").call(xAxisDetail);
+  }; 
+
 }
+
+
+//console.log(dataSet);
+
 
 
 
